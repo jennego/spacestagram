1c9a217e8da7b0a8d11998e972ea9fcd8c3be79e
Jennifer Chow <jennego@gmail.com>
2022-01-19 22:39:25 -0800

    fix modal image height

--- src/components/imageItem.js
+++ src/components/imageItem.js
@@ -8,12 +8,8 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import curiosity from "../avatars/rover-c.jpg";
 
 import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
-import Typography from "@mui/material/Typography";
 
 const ImageItem = (props) => {
   const [hover, setHover] = useState(false);
@@ -111,7 +107,7 @@ const ImageItem = (props) => {
             style={{
               objectFit: "contain",
               maxWidth: "80vw",
-              maxHeight: "80vh",
+              maxHeight: "79vh",
               marginBottom: "0.5rem",
             }}
           />
